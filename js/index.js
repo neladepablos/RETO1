672a9a5ae65fd71790bb3a9006e8fd36491b0690
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2020-11-12 16:31:18 -0500

    ultimos cambios boton

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@
 
 
 // -------------------------Pedir un texto, luego otro, concatenarlos y mostrarlos en un alert
-let text = prompt("Escribe 3 pelìculas sobre ciencia que te guste: ");
-let text2 = prompt("¿Cuàl es tu cientìfico favorito y por què?: ");
-let result = "Sus 3 peliulas de ciencia favoritas son: " + text + " y su cientifico favorito es: " + text2;
-console.log(result);
+// let text = prompt("Escribe 3 pelìculas sobre ciencia que te guste: ");
+// let text2 = prompt("¿Cuàl es tu cientìfico favorito y por què?: ");
+// let result = "Sus 3 peliulas de ciencia favoritas son: " + text + " y su cientifico favorito es: " + text2;
+// console.log(result);
