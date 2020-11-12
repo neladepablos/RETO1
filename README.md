0ba0ccf466bf8cff570573f399e497bd05baa506
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2020-11-12 16:16:54 -0500

    Algoritmo en JS simple

---
+++ README.md
@@ -0,0 +1,11 @@
+# Desafio1: Conceptos generales, sintaxis y variables
+_En esta clase de diò lo bàsico e inical de javascript, donde se dejò como desafio crear un algoritmo simple.
+
+## Construido con:
+ * HTML
+ *CSS
+ *Javascript
+
+
+ ---
+ ⌨️ con ❤️😊
---
+++ js/index.js
@@ -0,0 +1,19 @@
+// --------------------Pedir nombre y mostrarlo en consola.----------------------
+// let name = prompt("Ingresa tu nombre: ");
+// console.log("Hola," + " " + name);
+
+
+//-----------------------------Pedir un nùmero, sumarlo a otro y mostrarlo en consola.------------------
+// let number1 = Number(prompt("Ingresa el primer nùmero: "));
+// let number2 = Number(prompt("Ingresa el segundo nùmero: "));
+// let number3 = 15;
+
+// let result = number1 + number2 + number3;
+// console.log(result);
+
+
+// -------------------------Pedir un texto, luego otro, concatenarlos y mostrarlos en un alert
+let text = prompt("Escribe 3 pelìculas sobre ciencia que te guste: ");
+let text2 = prompt("¿Cuàl es tu cientìfico favorito y por què?: ");
+let result = "Sus 3 peliulas de ciencia favoritas son: " + text + " y su cientifico favorito es: " + text2;
+console.log(result);
